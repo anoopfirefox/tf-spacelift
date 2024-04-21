446c45b2f980cd84ff5faee6a2fdd3f9bf4601e1
anoop.hegde <anoop.techstorm@gmail.com>
2024-04-21 18:13:47 +0530

    updated provider.tf with shared credentials file

--- providers.tf
+++ providers.tf
@@ -8,6 +8,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-2"
+  shared_credentials_files = ["~/.aws/credentials"]
+  shared_config_files = ["~/.aws/config"]
   #shared_credentials_file = "~/.aws/credentials"
   #shared_config_file      = "~/.aws/config"
   profile = "aws-credentials"
